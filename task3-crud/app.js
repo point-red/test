4d9e7f0ec8c5fa21eba4adcf4e5f8e3b68b254a0
satriyoaji <satriyoaji27@gmail.com>
2021-10-19 18:27:49 +0700

    add update, delete, get id Users

--- task3-crud/app.js
+++ task3-crud/app.js
@@ -22,11 +22,62 @@ app.post('/users',
     return res.status(500).json(err)
   }
 })
+
 app.get('/users', async (req, res) => {
   try {
     const users = await User.findAll()
 
     return res.json(users)
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({ error: 'Something went wrong' })
+  }
+})
+
+app.get('/users/:id', async (req, res) => {
+  const id = req.params.id
+  try {
+    const user = await User.findOne({
+      where: { id },
+    })
+
+    return res.json(user)
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({ error: 'Something went wrong' })
+  }
+})
+
+app.delete('/users/:id', async (req, res) => {
+  const id = req.params.id
+  try {
+    const user = await User.findOne({ where: { id } })
+
+    await user.destroy()
+
+    return res.json({ message: 'User deleted!' })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({ error: 'Something went wrong' })
+  }
+})
+
+app.put('/users/:id',
+    body('name').exists().withMessage('must be exist with current Name string datatype'),
+    body('email').isEmail(),
+    async (req, res) => {
+  const id = req.params.id
+  const { name, email, age } = req.body
+  try {
+    const user = await User.findOne({ where: { id } })
+
+    user.name = name
+    user.email = email
+    user.age = age
+
+    await user.save()
+
+    return res.json(user)
   } catch (err) {
     console.log(err)
     return res.status(500).json({ error: 'Something went wrong' })

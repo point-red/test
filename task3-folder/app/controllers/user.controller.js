a7f96f4351ea738eb179bb55a930cc3c0ebb24a5
Fema F.P <63629593+femaflam22@users.noreply.github.com>
2021-11-08 14:05:08 +0700

    Create user.controller.js

---
+++ task3-folder/app/controllers/user.controller.js
@@ -0,0 +1,109 @@
+const User = require('../models/user.model.js');
+
+exports.create = (req, res) => {
+    if(!req.body.content) {
+        return res.status(400).send({
+            message: "Note content can not be empty"
+        });
+    }
+
+    const user = new User({
+        name: req.body.name, 
+        email: req.body.email,
+        age: req.body.age
+    });
+
+    user.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the user."
+        });
+    });
+};
+
+exports.findAll = (req, res) => {
+    User.find()
+    .then(users => {
+        res.send(users);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving users."
+        });
+    });
+};
+
+exports.findOne = (req, res) => {
+    User.findById(req.params.id)
+    .then(user => {
+        if(!user) {
+            return res.status(404).send({
+                message: "User not found with id " + req.params.id
+            });            
+        }
+        res.send(user);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "User not found with id " + req.params.id
+            });                
+        }
+        return res.status(500).send({
+            message: "Error retrieving user with id " + req.params.id
+        });
+    });
+};
+
+exports.update = (req, res) => {
+    // Validate Request
+    if(!req.body.content) {
+        return res.status(400).send({
+            message: "User content can not be empty"
+        });
+    }
+
+    User.findByIdAndUpdate(req.params.id, {
+        name: req.body.name, 
+        email: req.body.email,
+        age: req.body.age
+    }, {new: true})
+    .then(user => {
+        if(!user) {
+            return res.status(404).send({
+                message: "User not found with id " + req.params.id
+            });
+        }
+        res.send(user);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "User not found with id " + req.params.id
+            });                
+        }
+        return res.status(500).send({
+            message: "Error updating user with id " + req.params.id
+        });
+    });
+};
+
+exports.delete = (req, res) => {
+    User.findByIdAndRemove(req.params.id)
+    .then(user => {
+        if(!user) {
+            return res.status(404).send({
+                message: "User not found with id " + req.params.id
+            });
+        }
+        res.send({message: "User deleted successfully!"});
+    }).catch(err => {
+        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+            return res.status(404).send({
+                message: "User not found with id " + req.params.id
+            });                
+        }
+        return res.status(500).send({
+            message: "Could not delete user with id " + req.params.id
+        });
+    });
+};
